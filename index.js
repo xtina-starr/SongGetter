295a28b9d9d2bff2e30f435b999c26bbca125978
Christina Thompson <c.thompsonms@gmail.com>
2015-03-23 21:06:15 -0700

    added entrypoint and create web server

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    CronJob = require('cron').CronJob,
+    request = require('request');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('okay');
+});
+
+server.listen(1337, '127.0.0.1');
+
