d97b401c4c13f32c45ac2d1ce15a64f79311f5df
Christina Thompson <c.thompsonms@gmail.com>
2015-03-23 21:56:59 -0700

    make API call, select two most popular songs, save to firebase db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http'),
     CronJob = require('cron').CronJob,
-    request = require('request');
+    request = require('request'),
+    _und = require('underscore'),
+    Firebase = require('firebase');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -11,5 +13,47 @@ server.listen(1337, '127.0.0.1');
 
 new CronJob('* * * * * *', function() {
   console.log("This job will run daily");
-})
+
+  request.get("http://api.thisismyjam.com/1/explore/popular.json", function(error, response, body) {
+
+    if(!error && response.statusCode == 200) {
+      var parsedResult = JSON.parse(body).jams;
+
+      var sorted = _und.sortBy(parsedResult, function(item, i) {
+        return -item.likesCount;
+      }).slice(0,2);
+
+      console.log(sorted);
+
+      var ref = new Firebase();
+      var ts = new Date().toString();
+
+      _und.each(sorted, function(item, i) {
+        ref.push({
+          idOnJams: item.id,
+          title: item.title,
+          artist: item.artist,
+          mediumImage: item.jamvatarMedium,
+          largeImage: item.jamvatarLarge,
+          url: item.viaUrl,
+          via: item.via,
+          likes: item.likesCount,
+          commentsCount: item.commentsCount,
+          rejamsCount: item.rejamsCount,
+          createdAt: ts
+        }, function(error) {
+          if(error) {
+            console.log("There was an error: " + error);  
+          }else{
+            console.log("Successfully Saved")
+          }
+        });
+
+      });
+      console.log("*************************");
+    }
+
+  });
+
+}, null, true, "America/Los_Angeles");
 
