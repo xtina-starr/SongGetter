00a4715bfefc3d4c90d456b65e8236443f184296
Christina Thompson <c.thompsonms@gmail.com>
2015-05-06 15:54:47 -0700

    created two separate functions for cleaner code

--- index.js
+++ index.js
@@ -15,7 +15,40 @@ console.log("server is started");
 
 server.listen(1337, '127.0.0.1');
 
-new CronJob('0 30 20 * * *', function() {
+function grabTopTwo(arr) {
+  return _und.sortBy(arr, function(item, i) {
+        return -item.likesCount;
+      }).slice(0,2);
+}
+
+function saveToFireBase(jams) {
+  var ref = new Firebase(firebaseUrl);
+  var ts = new Date().toString();
+  _und.each(jams, function(item, i) {
+    ref.push({
+      createdAt: ts,
+      idOnJams: item.id,
+      title: item.title,
+      artist: item.artist,
+      mediumImage: item.jamvatarMedium,
+      largeImage: item.jamvatarLarge,
+      url: item.viaUrl,
+      via: item.via,
+      likes: item.likesCount,
+      commentsCount: item.commentsCount,
+      rejamsCount: item.rejamsCount
+    }, function(error) {
+      if(error) {
+        console.log("There was an error: " + error);  
+      }else{
+        console.log("Successfully Saved")
+      }
+    });
+
+  });
+}
+
+new CronJob('0 50 15 * * *', function() {
   console.log("This job will run daily");
 
   request.get("http://api.thisismyjam.com/1/explore/popular.json", function(error, response, body) {
@@ -23,38 +56,9 @@ new CronJob('0 30 20 * * *', function() {
     if(!error && response.statusCode == 200) {
       var parsedResult = JSON.parse(body).jams;
 
-      var sorted = _und.sortBy(parsedResult, function(item, i) {
-        return -item.likesCount;
-      }).slice(0,2);
-
-      console.log(sorted);
-
-      var ref = new Firebase(firebaseUrl);
-
-      var ts = new Date().toString();
-
-      _und.each(sorted, function(item, i) {
-        ref.push({
-          createdAt: ts,
-          idOnJams: item.id,
-          title: item.title,
-          artist: item.artist,
-          mediumImage: item.jamvatarMedium,
-          largeImage: item.jamvatarLarge,
-          url: item.viaUrl,
-          via: item.via,
-          likes: item.likesCount,
-          commentsCount: item.commentsCount,
-          rejamsCount: item.rejamsCount
-        }, function(error) {
-          if(error) {
-            console.log("There was an error: " + error);  
-          }else{
-            console.log("Successfully Saved")
-          }
-        });
-
-      });
+      var topTwoJams = grabTopTwo(parsedResult);
+      console.log(topTwoJams);
+      saveToFireBase(topTwoJams);
     }
 
   });
