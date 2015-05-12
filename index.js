ef72469faf8d9ec9c3075579970ff44c9b451a56
Christina Thompson <c.thompsonms@gmail.com>
2015-05-11 20:38:32 -0700

    just manually testing the view

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var http = require('http'),
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
+  res.end('Hello World\n');
 });
 console.log("server is started");
 
